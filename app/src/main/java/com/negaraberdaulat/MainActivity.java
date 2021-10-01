43a568bbad0c5ed344f7c14e42e6489298edad34
AchmadSirojudin <achmadsiroj07@gmail.com>
2021-10-01 14:33:25 +0700

    +listview +spinner

--- app/src/main/java/com/negaraberdaulat/MainActivity.java
+++ app/src/main/java/com/negaraberdaulat/MainActivity.java
@@ -13,99 +13,129 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String mTitle[] = {"Amerika", "Argentina", "Australia", "Belanda", "Belgia", "Brazil", "China", "India", "Indonesia", "Jepang", "Jerman"};
-    String sDeskripsi[] = {"Amerika Description", "Argentina Description", "Australia Description", "Belanda Description", "Belgia Description", "Brazil Description", "China Description", "India Description", "Indonesia Description", "Jepang Description", "Jerman Description"};
+    Spinner spinner;
+
+    String mTitle[] = {"Amerika Serikat", "Argentina", "Australia", "Belanda", "Belgia", "Brazil", "China", "India", "Indonesia", "Jepang", "Jerman"};
+    String mDescription[] = {"Amerika Description", "Argentina Description", "Australia Description", "Belanda Description", "Belgia Description", "Brazil Description", "China Description", "India Description", "Indonesia Description", "Jepang Description", "Jerman Description"};
     int images[] = {R.drawable.amerika, R.drawable.argentina, R.drawable.australia, R.drawable.belanda, R.drawable.belgia, R.drawable.brazil, R.drawable.china, R.drawable.india, R.drawable.indonesia, R.drawable.jepang, R.drawable.jerman,};
+    // so our images and other things are set in array
+
+    // now paste some images in drawable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String negara = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Anda klik negara: " +negara, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         listView = findViewById(R.id.listView);
+        // now create an adapter class
 
-        MyAdapter adapter = new MyAdapter(this, mTitle, sDeskripsi, images);
-        listView.setAdapter(adapter);
+        MyAdapter adapter1 = new MyAdapter(this, mTitle, mDescription, images);
+        listView.setAdapter(adapter1);
+        // there is my mistake...
+        // now again check this..
 
+        // now set item click on list view
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Amerika Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Argentina Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Australia Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Belanda Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Belgia Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Brazil Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "China Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "India Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Indonesia Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Jepang Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position == 0){
-                    Toast.makeText(MainActivity.this, "Jerman Description", Toast.LENGTH_SHORT).show();
-                }
-
-
-        };
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Amerika Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Argentina Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Australia Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Belanda Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Belgia Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Brazil Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "China Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "India Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Indonesia Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Jepang Description", Toast.LENGTH_SHORT).show();
+                    }
+                    if (position == 0){
+                        Toast.makeText(MainActivity.this, "Jerman Description", Toast.LENGTH_SHORT).show();
+                    }
+            }
         });
+        // so item click is done now check list view
     }
 
     class MyAdapter extends ArrayAdapter<String> {
+
         Context context;
-        String rTittle[];
+        String rTitle[];
         String rDescription[];
         int rImgs[];
 
-        MyAdapter (Context c, String title[], String description[], int img[]) {
+        MyAdapter (Context c, String title[], String description[], int imgs[]) {
             super(c, R.layout.row, R.id.textView1, title);
             this.context = c;
-            this.rTittle = title;
+            this.rTitle = title;
             this.rDescription = description;
-            this.rImgs = img;
+            this.rImgs = imgs;
 
         }
 
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater lalyoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = lalyoutInflater.inflate(R.layout.row, parent, false);
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
             ImageView images = row.findViewById(R.id.image);
             TextView myTitle = row.findViewById(R.id.textView1);
             TextView myDescription = row.findViewById(R.id.textView2);
 
+            // now set our resources on views
             images.setImageResource(rImgs[position]);
-            myTitle.setText(rTittle[position]);
+            myTitle.setText(rTitle[position]);
             myDescription.setText(rDescription[position]);
 
 
 
-            return super.getView(position, convertView, parent);
+
+            return row;
         }
     }
     }
